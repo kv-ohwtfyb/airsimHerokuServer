f63fd07cf1483d580efcdb8ac6b0e9a4291c6023
unknown <ingenzivany@gmail.com>
2020-08-04 16:33:37 +0200

    make it better

--- server.py
+++ server.py
@@ -1,14 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, join_room, send, emit
-#from os import path
+import os
+import sqlite3
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+messages = [] #It's a list containing the activities that happened on the server
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(base_dir, 'server/database/dbTesting.sqlite')
+
 @app.route("/")
 def helloWorld():
-    return "Hello World"
+    """
+    Returns a page containing all the activities that went on since the last launch of the server
+    """
+    return render_template("index.html", messages=messages)
 
 @socketio.on("join")
 def when_join(data):
@@ -18,14 +27,22 @@ def when_join(data):
     A dict {"password":"id"}
     :return: None, a boolean method but does what's necessarily for the connection.
     """
-    username = data['password']
-    if username == "vany":
-        room = "Room01"
-        join_room(room)
-        send(username + ' has entered the room.', room=room, broadcast=True)
-    room = "Kitchen"
-    join_room(room)
-    send(username + ' has entered the room.', room=room, broadcast=True)
+    try: #if it's a client
+        username = data['password']
+        rooms = findRoomForClient(username)
+        if rooms:
+            for room in rooms:
+                join_room(room)
+                send(username + ' has entered the ' + room, room=room, broadcast=True)
+                messages.append(username + ' has entered the ' + room)
+
+    except: #if it's a station
+        username = data['id']
+        room = findRoomForStation(username)
+        if room:
+            messages.append(username + ' has entered the ' + room)
+            join_room(room)
+            send(username + ' has entered the' + room , room=room, broadcast=True)
 
 @socketio.on('station')
 def stationData(data):
@@ -34,7 +51,45 @@ def stationData(data):
     :param data:
     :return:
     """
-    emit(data["id"] + ' has sent : '+ data["data"], room=data["room"], broadcast=True)
+    room = findRoomForStation(data["id"])
+    send(data["id"] + ' has sent : '+ data["data"], room=findRoomForStation(data["id"]), broadcast=True)
+    messages.append(data["id"] + ' has sent : '+ data["data"] + "to the" + room )
+
+def findRoomForStation(stationId):
+    """
+    This functions return the string of the room that's in the database
+    :param stationId: a string, which is the stationId
+    :return a string, else None
+    """
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT roomId FROM rooms WHERE stations=(?)",(stationId,))
+        result = cursor.fetchone()
+        conn.close()
+        if result:
+            return result[0]
+        else:
+            return None
+    except Exception as e:
+        messages.append(str(e))
+
+def findRoomForClient(password):
+    """
+    This functions returns the tuple of the rooms that client has acces to
+    in the database
+    :param tuple: a string, which is the password
+    :return a tuple, else None
+    """
+    try:
+        conn = sqlite3.connect(db_path)
+        cursor = conn.cursor()
+        cursor.execute("SELECT DISTINCT(rooms) FROM users WHERE password=(?)",(password,))
+        result = cursor.fetchone()
+        conn.close()
+        return result
+    except Exception as e:
+        messages.append(str(e))
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
