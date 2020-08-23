cba09113a5d1cb141d2973d1da3c5d385caaf77f
unknown <ingenzivany@gmail.com>
2020-08-23 15:20:40 +0200

    Added the rest of the protocols

--- server.py
+++ server.py
@@ -38,55 +38,40 @@ def when_join(data):
     if "password" in data.keys(): #if it's a client
         password = data['password']
         user_object = findClient(password)
-        if user_object:
-            dict = {"admin":1 if user_object.admin else 0}
-            rooms = []
-
-            for roomId in user_object.rooms:
-                room_object = findRoom(roomId)
-                if room_object:
-                    join_room(roomId) #Adds the user in a room
-                    room_dict = {"id":roomId}
-                    stations_list = []
-
-                    for stationId in room_object.stations:
-                        station_object = findStation(stationId)
-                        if station_object:
-                            station_dict = {"id":str(stationId),
-                                            "sensors": station_object.sensors}
-                            stations_list.append(station_dict)
-
-                    room_dict["stations"] = stations_list
-                    rooms.append(room_dict)
-
-            dict["rooms"]=rooms
-            send("sending")
+        if user_object: #Meaning the user exists in the database
+            dict = initiationDataForApp(user_object)
             emit("initial", dict)
         else:
             disconnect() #Disconnect the user if he is not recognised
-
 
     elif "station" in data.keys(): #if it's a station
         username = data['station']
         room = findRoomForStation(username)
         if room:
             join_room(room)
-            send(username + ' has entered the' + room , room=room, broadcast=True)
-    elif "room" in data.keys(): #If it's a Room Tab
-        pass
+            emit("initial", {"room":room})
+        else:
+            disconnect() #Disconnect the user if he is not recognised
+
+    elif "room" in data.keys() and "password" in data.keys(): #If it's a Room Tab
+        password = data['password']
+        user_object = findClient(password)
+        if data["room"] in user_object.rooms :
+            initiationDataForRoomTab(data["room"])
+        else:
+            disconnect()
 
     else: #Unkown protocol
         disconnect()
 
-@socketio.on('station')
+@socketio.on('data')
 def stationData(data):
     """
     This event is when a station sends in data
-    :param data:
-    :return:
+    :param data: data received from the stations.
+    :return: None
     """
-    room = findRoomForStation(data["id"])
-    send(data["id"] + ' has sent : '+ data["data"], room=findRoomForStation(data["id"]), broadcast=True)
+    emit("data", data, room=data["room"])
 
 def findRoomForStation(stationId):
     """
@@ -95,17 +80,15 @@ def findRoomForStation(stationId):
     :return a string, else None
     """
     try:
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("SELECT roomId FROM rooms WHERE stations=(?)",(stationId,))
-        result = cursor.fetchone()
-        conn.close()
-        if result:
-            return result[0]
-        else:
-            return None
+        room_objects = models.Room.query.filter_by().all()
+        for room in room_objects:
+            if stationId in room.stations:
+                return room.id
+        return
+
     except Exception as e:
         print(e)
+
 
 def findClient(password):
     """
@@ -130,7 +113,7 @@ def findRoom(roomId):
     :return: Room model if it exists else None.
     """
     try:
-        # Checks if the user exists
+        # Checks if the room exists
         room_object = models.Room.query.filter_by(id=roomId).first()
         return room_object
 
@@ -146,11 +129,63 @@ def findStation(stationId):
     """
     try:
         # Checks if the user exists
-        room_object = models.Station.query.filter_by(id=stationId).first()
-        return room_object
+        station_object = models.Station.query.filter_by(id=stationId).first()
+        return station_object
 
     except Exception as e:
         print(e)
+
+def initiationDataForApp(user_object):
+    """
+    The function loads up all the initial data to send back to the App.
+    :param user_object: A model object containing his row in the user database table
+    :return: a dict, containing all the infos.
+    """
+    dict = {"admin": 1 if user_object.admin else 0}
+    rooms = []
+
+    for roomId in user_object.rooms:
+        room_object = findRoom(roomId)
+        if room_object:
+            join_room(roomId)  # Adds the socket in a room
+            room_dict = {"id": roomId}
+            stations_list = []
+
+            for stationId in room_object.stations:
+                station_object = findStation(stationId)
+                if station_object:
+                    station_dict = {"id": str(stationId),
+                                    "sensors": station_object.sensors}
+                    stations_list.append(station_dict)
+
+            room_dict["stations"] = stations_list
+            rooms.append(room_dict)
+
+    dict["rooms"] = rooms
+    return dict
+
+def initiationDataForRoomTab(roomId):
+    """
+    The function loads up all the initial data to send back to the App.
+    :param user_object: a row in the user table, database.
+    :param room: the string of the room id.
+    :return: a dict, containing all the infos.
+    """
+    room_object = findRoom(roomId)
+    if room_object:
+        join_room(roomId)  # Adds the socket in a room
+        room_dict = {"id": roomId}
+        stations_list = []
+
+        for stationId in room_object.stations:
+            station_object = findStation(stationId)
+            if station_object:
+                station_dict = {"id": str(stationId),
+                                "sensors": station_object.sensors}
+                stations_list.append(station_dict)
+
+        room_dict["stations"] = stations_list
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
